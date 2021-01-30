85682cb9186db10a502561f23da7e735601a0c2a
Luiz Henrique Santos <lhsantosazs@gmail.com>
2021-01-30 09:32:44 -0300

    Adding models

---
+++ src/app/Models/Race.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Race extends Model
+{
+    protected $table = 'race';
+}
---
+++ src/app/Models/RaceRunner.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RaceRunner extends Model
+{
+    protected $table = 'race_runner';
+}
